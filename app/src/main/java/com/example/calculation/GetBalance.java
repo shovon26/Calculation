e48df5b373af91756d1bc97bb5f80d773c93e78f
shovon26 <djshovon12@gmail.com>
2022-09-09 15:50:46 +0600

    Refactor the code

--- app/src/main/java/com/example/calculation/GetBalance.java
+++ app/src/main/java/com/example/calculation/GetBalance.java
@@ -1,5 +1,7 @@
 package com.example.calculation;
 
+import android.util.Log;
+
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
@@ -11,6 +13,7 @@ import java.math.RoundingMode;
 import java.util.concurrent.TimeUnit;
 
 public class GetBalance {
+    private static String TAG = "GetBalance";
 
     public static BigDecimal getBalance(String ethAddress) {
         BigDecimal scaledBalance = new BigDecimal(0.0);
@@ -26,16 +29,16 @@ public class GetBalance {
                     .ethGetBalance(ethAddress, DefaultBlockParameter.valueOf("latest")).sendAsync()
                     .get(10, TimeUnit.SECONDS);
 
+
             final BigInteger unscaledBalance = balanceResponse.getBalance();
 
             scaledBalance = new BigDecimal(unscaledBalance)
                     .divide(new BigDecimal(1000000000000000000L), 18, RoundingMode.HALF_UP);
 
-            System.out.println("Balance then :: " + scaledBalance);   //Scaled Balance is not found here
+            Log.d(TAG, "Balance then :: " + scaledBalance);   //Scaled Balance is not found here
 
         } catch (Exception ex) {
-            ex.printStackTrace();
-            //System.out.println("Error");
+            Log.d(TAG, "error: " + ex.getMessage());
         }
         return scaledBalance;
     }
--- app/src/main/java/com/example/calculation/MainActivity.java
+++ app/src/main/java/com/example/calculation/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
 
     EditText editText;
     TextView textView;
@@ -29,10 +31,12 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(ethAddress);
-                BigDecimal balance = GetBalance.getBalance(ethAddress);
-                System.out.println("Balance now :: " + balance);
-                textView.setText(balance +"");
+                // editText.setText(ethAddress);
+                BigDecimal balance = GetBalance.getBalance(editText.getText().toString());
+                Log.d(TAG, "Balance now :: " + balance);
+                Log.d(TAG, "address:: " + editText.getText());
+                editText.setText("");
+                textView.setText("Balance " + balance.toString());
             }
         });
     }
